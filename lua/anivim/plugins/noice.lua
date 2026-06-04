7e8c9a638a00653704a2b11f9440a91164fc08d5
Roshan Iruku <roshaniruku@gmail.com>
2026-06-03 23:47:53 -0400

    added

--- lua/anivim/plugins/noice.lua
+++ lua/anivim/plugins/noice.lua
@@ -1,14 +1,23 @@
 return -- lazy.nvim
 {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-        timeout = 1000,
-
-        -- add any options here
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            views = {
+                notify = { timeout = 50, focusable = false },
+                mini = { timeout = 50 },
+            },
+        },
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        },
     },
-    dependencies = {
-        "MunifTanjim/nui.nvim",
+    {
         "rcarriga/nvim-notify",
+        opts = {
+            background_colour = "#000000",
+        },
     },
 }
